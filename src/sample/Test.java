075a5e6dcf14702deddb60e89f842ed91f3f0187
Alejandro <ct.rodriguezAle@gmail.com>
2020-03-31 20:50:32 -0300

    Generacion de listas de aleatorios
    ya hice los metodos que generan las listas en funcion de los valores ingresados por la UI.. en la clase MainCOntroller

--- src/sample/Test.java
+++ src/sample/Test.java
@@ -1,10 +1,15 @@
-package sample.model;
+package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.controller.MainController;
+import sample.model.GeneradorAleatorioJava;
+import sample.model.GeneradorCongruencialLineal;
+import sample.model.GeneradorCongruencialMultiplicativo;
+import sample.model.IGeneradorAleatorio;
 
 public class Test extends Application {
 
@@ -18,7 +23,12 @@ public class Test extends Application {
 
 
     public static void main(String[] args) {
-        IGeneradorAleatorio generador = new GeneradorCongruencialLineal(95,31,17,653);
+        MainController controlador = new MainController();
+        controlador.opcionGenerarSecuenciaDeAleatoriosCongruencialLineal(20,95,31,17,653);
+        controlador.opcionGenerarSecuenciaDeAleatoriosCongruencialMultiplicativo(20,95,31,653);
+        controlador.opcionGenerarSecuenciaDeAleatoriosJava(20);
+
+        /*IGeneradorAleatorio generador = new GeneradorCongruencialLineal(95,31,17,653);
         System.out.println("Generador Congruancial Lineal");
         for (int i = 0; i < 10; i++) {
             System.out.println("    aleatorio: " +  generador.GenerarAleatorio() + " - semilla: "+ generador.getSemilla());
@@ -33,6 +43,6 @@ public class Test extends Application {
         generador = new GeneradorAleatorioJava();
         for (int i = 0; i < 10; i++) {
             System.out.println("    aleatorio: " +  generador.GenerarAleatorio() + " - semilla: "+ generador.getSemilla());
-        }
+        }*/
     }
 }
--- src/sample/controller/MainController.java
+++ src/sample/controller/MainController.java
@@ -1,4 +1,84 @@
 package sample.controller;
 
-public class MainController {
+import sample.model.GeneradorAleatorioJava;
+import sample.model.GeneradorCongruencialLineal;
+import sample.model.IGeneradorAleatorio;
+
+public class MainController
+{
+    private int tamañoMuestra;
+    private int semillaIngresada;
+    private int moduloIngresado;
+    private int multiplicadorIngresado;
+    private int incrementoIngresado;
+    private IGeneradorAleatorio generador;
+    private double[] listaAleatorios;
+    private int[] listaSemillas;
+
+    public MainController()
+    {
+
+    }
+
+    public void opcionGenerarSecuenciaDeAleatoriosJava(int aTamañoMuestra)
+    {
+        generador = new GeneradorAleatorioJava();
+        tamañoMuestra = aTamañoMuestra;
+
+        generarAleatorios();
+    }
+
+    public void opcionGenerarSecuenciaDeAleatoriosCongruencialLineal
+            (int aTamañoMuestra, int aSemilla, int aMultiplicador, int aIncremento, int aModulo)
+    {
+        this.tamañoMuestra = aTamañoMuestra;
+        this.semillaIngresada = aSemilla;
+        this.multiplicadorIngresado = aMultiplicador;
+        this.incrementoIngresado = aIncremento;
+        this.moduloIngresado = aModulo;
+
+        generador = new GeneradorCongruencialLineal(semillaIngresada, multiplicadorIngresado, incrementoIngresado, moduloIngresado);
+
+        generarAleatorios();
+    }
+
+    public void opcionGenerarSecuenciaDeAleatoriosCongruencialMultiplicativo
+            (int aTamañoMuestra, int aSemilla, int aMultiplicador, int aModulo)
+    {
+        this.tamañoMuestra = aTamañoMuestra;
+        this.semillaIngresada = aSemilla;
+        this.multiplicadorIngresado = aMultiplicador;
+        this.moduloIngresado = aModulo;
+
+        generador = new GeneradorCongruencialLineal(semillaIngresada, multiplicadorIngresado, incrementoIngresado, moduloIngresado);
+
+        generarAleatorios();
+    }
+
+    public void generarAleatorios()
+    {
+
+        if (generador != null )
+        {
+            listaAleatorios = new double[tamañoMuestra];
+            listaSemillas = new int[tamañoMuestra];
+            for (int i = 0; i < tamañoMuestra; i++)
+            {
+                listaAleatorios[i] = generador.GenerarAleatorio();
+                listaSemillas[i]   = generador.getSemilla();
+            }
+        } else
+        {
+
+        }
+        mostrar();
+    }
+
+    public void mostrar()
+    {
+        for (int i = 0; i < tamañoMuestra; i++) {
+            System.out.println(listaAleatorios[i] + " " +listaSemillas[i]);
+        }
+
+    }
 }
