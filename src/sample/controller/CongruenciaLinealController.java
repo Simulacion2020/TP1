1a7738550056284ba6aa5ea4b25949f24e7cc538
Esteban Campana <esteb.camp89@gmail.com>
2020-04-07 01:17:44 -0300

    histograma random

--- src/sample/controller/CongruenciaLinealController.java
+++ src/sample/controller/CongruenciaLinealController.java
@@ -8,13 +8,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.MapValueFactory;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 import javafx.util.StringConverter;
@@ -26,6 +28,7 @@ import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class CongruenciaLinealController {
 
@@ -36,8 +39,6 @@ public class CongruenciaLinealController {
     TextField clinealtext;
     @FXML
     TextField klinealtext;
-    //    @FXML
-//    TextField mlinealtext;
     @FXML
     TextField glinealtext;
     @FXML
@@ -52,14 +53,21 @@ public class CongruenciaLinealController {
     TableColumn xiCol;
     @FXML
     TableColumn xi1Col;
-
     @FXML
     GridPane root;
+    @FXML
+    Button buttonChiLineal;
+    @FXML
+    Button buttonHistogramaLineal;
 
 
     public static final String Column1MapKey = "i";
     public static final String Column2MapKey = "xi";
     public static final String Column3MapKey = "xi+1";
+
+    int DATA_SIZE = 10;
+    float data[] = new float[DATA_SIZE];
+    int group[] = new int[10]; //cantidad de intervalos
 
 
     @FXML
@@ -77,6 +85,128 @@ public class CongruenciaLinealController {
             camposInvalidosLinealLabel.setText("");
             System.out.println("Campos validos, realizamos calculos para generar tabla");
             this.generarTabla();
+            buttonChiLineal.setDisable(false);
+            buttonHistogramaLineal.setDisable(false);
+        }
+    }
+
+    @FXML
+    protected void handleHistogramaButtonAction(ActionEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        URL urlLocation = getClass().getResource("/sample/view/histogramaView.fxml");
+        fxmlLoader.setLocation(urlLocation);
+
+        try {
+            AnchorPane anchorPane = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Histograma");
+            Scene scene = generarHistograma();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Scene generarHistograma() {
+        prepareData();
+        groupData();
+
+        Label labelInfo = new Label();
+        labelInfo.setText("Histograma Congruencia lineal");
+
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        final BarChart<String, Number> barChart =
+                new BarChart<>(xAxis, yAxis);
+        barChart.setCategoryGap(0);
+        barChart.setBarGap(0);
+
+        xAxis.setLabel("Rango");
+        yAxis.setLabel("Cantidad de numeros aleatorios");
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("Frecuencia");
+        series1.getData().add(new XYChart.Data("0-0.1", group[0]));
+        series1.getData().add(new XYChart.Data("0.1-0.2", group[1]));
+        series1.getData().add(new XYChart.Data("0.2-0.3", group[2]));
+        series1.getData().add(new XYChart.Data("0.3-0.4", group[3]));
+        series1.getData().add(new XYChart.Data("0.4-0.5", group[4]));
+
+        series1.getData().add(new XYChart.Data("0.5-0.6", group[5]));
+        series1.getData().add(new XYChart.Data("0.6-0.7", group[6]));
+        series1.getData().add(new XYChart.Data("0.7-0.8", group[7]));
+        series1.getData().add(new XYChart.Data("0.8-0.9", group[8]));
+        series1.getData().add(new XYChart.Data("0.9-1.0", group[9]));
+
+        barChart.getData().addAll(series1);
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(labelInfo, barChart);
+
+        StackPane root = new StackPane();
+        root.getChildren().add(vBox);
+
+        Scene scene = new Scene(root, 800, 450);
+
+        return scene;
+    }
+
+    //generate dummy random data
+    private void prepareData() {
+
+        Random random = new Random();
+        for (int i = 0; i < DATA_SIZE; i++) {
+            data[i] = random.nextFloat();
+            System.out.println(data[i]);
+        }
+    }
+
+    //count data population in groups
+    private void groupData() {
+        for (int i = 0; i < 10; i++) {
+            group[i] = 0;
+        }
+        for (int i = 0; i < DATA_SIZE; i++) {
+            if (data[i] <= 0.1) {
+                group[0]++;
+            } else if (data[i] <= 0.2) {
+                group[1]++;
+            } else if (data[i] <= 0.3) {
+                group[2]++;
+            } else if (data[i] <= 0.4) {
+                group[3]++;
+            } else if (data[i] <= 0.5) {
+                group[4]++;
+            } else if (data[i] <= 0.6) {
+                group[5]++;
+            } else if (data[i] <= 0.7) {
+                group[6]++;
+            } else if (data[i] <= 0.8) {
+                group[7]++;
+            } else if (data[i] <= 0.9) {
+                group[8]++;
+            } else if (data[i] <= 1) {
+                group[9]++;
+            }
+        }
+    }
+
+
+    @FXML
+    protected void handleChi2ButtonAction(ActionEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        URL urlLocation = getClass().getResource("/sample/view/chicuadradoView.fxml");
+        fxmlLoader.setLocation(urlLocation);
+        try {
+            AnchorPane anchorPane = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Prueba Chi²");
+            Scene scene = new Scene(anchorPane);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
